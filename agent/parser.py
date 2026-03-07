725bcae58b163d58c8484cc27d8f3964e39c377d
Mohit Nagda <156638554+mohitt38@users.noreply.github.com>
2026-03-07 14:40:24 +0530

    Update parser.py

--- agent/parser.py
+++ agent/parser.py
@@ -9,11 +9,11 @@ def parse_problem(problem_text):
     numbers = [int(n) for n in numbers]
 
     # example: "a number plus 5 is 10"
-    if "plus" in text and "is" in text and len(numbers) == 2:
+    if "plus"or "add" in text and "is" in text and len(numbers) == 2:
         return f"x + {numbers[0]} = {numbers[1]}"
 
     # example: "a number minus 3 is 7"
-    if "minus" in text and "is" in text and len(numbers) == 2:
+    if "minus" or "subtract" in text and "is" in text and len(numbers) == 2:
         return f"x - {numbers[0]} = {numbers[1]}"
 
     return None
